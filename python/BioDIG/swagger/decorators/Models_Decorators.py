8fe7e079e722a243f3676ff1afa090faee8462fc
Dan <ruhedr@muohio.edu>
2013-10-23 20:34:04 -0400

    Committing Code

---
+++ python/BioDIG/swagger/decorators/Models_Decorators.py
@@ -0,0 +1,50 @@
+#!usr/bin/python
+
+#####Decorators for Tags and Tag Groups
+#####apiID, name overlaps with both Tags and Tag Groups
+def Property_color(fn,TagColor)
+    fn_TagColor = TagColor
+    return fn
+
+def Property_name(fn, name)
+    fn_name = name
+    return fn
+
+def Property_point(fn, point)
+    fn_point = point
+    return fn
+
+def Property_dateCreated(fn, dateCreated)
+    fn_dateCreated = dateCreated
+    return fn
+
+def Property_id(fn, apiID)
+    fn_apiID = apiID
+    return fn
+
+
+def Property_imageid(fn, imageid)
+    fn_imageid = imageid
+    return fn
+
+def Property_lastModified(fn, lastModified)
+    fn_lastModified = lastModified
+    return fn
+
+def Property_dateCreated(fn, dateCreated)
+    fn_dateCreated = dateCreated
+    return fn
+
+def Property_user(fn, user)
+    fn_user = user
+    return fn
+
+def Property_isPrivate(fn, isPrivate)
+    fn_isPrivate = isPrivate
+    return fn
+
+def Property_(fn,)
+    fn_ =
+    return fn
+
+
