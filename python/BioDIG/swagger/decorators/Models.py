dd4636f9db9dde237e6e9f87fbd2d2af728f4c3c
Dan <ruhedr@muohio.edu>
2013-10-27 12:39:46 -0400

    Tidying up Code

--- python/BioDIG/swagger/decorators/Models.py
+++ python/BioDIG/swagger/decorators/Models.py
@@ -3,48 +3,42 @@
 #####Decorators for Tags and Tag Groups
 #####apiID, name overlaps with both Tags and Tag Groups
 def Property_color(fn,TagColor)
-    fn_TagColor = TagColor
+    fn.__TagColor = TagColor
     return fn
 
 def Property_name(fn, name)
-    fn_name = name
+    fn.__name = name
     return fn
 
 def Property_point(fn, point)
-    fn_point = point
+    fn.__point = point
     return fn
 
 def Property_dateCreated(fn, dateCreated)
-    fn_dateCreated = dateCreated
+    fn.__dateCreated = dateCreated
     return fn
 
 def Property_id(fn, apiID)
-    fn_apiID = apiID
+    fn.__apiID = apiID
     return fn
 
 
 def Property_imageid(fn, imageid)
-    fn_imageid = imageid
+    fn.__imageid = imageid
     return fn
 
 def Property_lastModified(fn, lastModified)
-    fn_lastModified = lastModified
+    fn.__lastModified = lastModified
     return fn
 
 def Property_dateCreated(fn, dateCreated)
-    fn_dateCreated = dateCreated
+    fn.__dateCreated = dateCreated
     return fn
 
 def Property_user(fn, user)
     fn_user = user
     return fn
 
-def Property_isPrivate(fn, isPrivate)
-    fn_isPrivate = isPrivate
+def Property_isPrivate(fn, isPrivate = true)
+    fn.__isPrivate = isPrivate
     return fn
-
-def Property_(fn,)
-    fn_ =
-    return fn
-
-
--- python/BioDIG/swagger/decorators/Parameters.py
+++ python/BioDIG/swagger/decorators/Parameters.py
@@ -1,49 +1,42 @@
 #!usr/bin/python
 
 ##Decorators for the parameters
-parameters = 
-[
-    
+
+
     def paramType_path(fn, path)
-       fn_path = path
+       fn.__path = path
        return fn
 
     def paramType_query(fn, query)
-       fn_query = query
+       fn.__query = query
        return fn
 
     def paramType_body(fn, body)
-       fn_body = body
+       fn.__body = body
        return fn
 
     def paramType_form(fn, form)
-       fn_form = form
+       fn.__form = form
        return fn
 
     def name(fn, name)
-       fn_name = name
+       fn.__name = name
        return fn
 
     def description(fn, description)
-       fn_description = description
+       fn.__description = description
        return fn
     
     def datatype(fn, datatype)
-       fn_datatype = datatype
+       fn.__datatype = datatype
        return fn
 
     def format(fn, APIformat)
-        fn_APIformat = APIformat
+        fn.__APIformat = APIformat
         return fn
 
     ##Decorator for the required field being true
-    def required_true(fn, required)
-        required = true
-        fn_required = required
+    def required_true(fn, required = true)
+        fn.__required = required
         return fn
 
-    def required(fn, required)
-        fn_required = required
-        return fn
-    
-]
