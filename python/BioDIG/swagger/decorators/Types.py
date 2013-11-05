d3a82ec45376e1d1de447df19136fa6521f0ad8b
Dan <ruhedr@muohio.edu>
2013-11-04 21:28:32 -0500

    Code Pushing

--- python/BioDIG/swagger/decorators/Types.py
+++ python/BioDIG/swagger/decorators/Types.py
@@ -1,10 +1,52 @@
 #!usr/bin/python
 
 ###Includes the conversion for types
+String = 'string'
+Boolean = 'boolean'
+Date = 'date'
+Integer = 'int'
+dateTime = 'date-time'
 
-def String = string;
-def Boolean = boolean;
-def Date = date;
-def Integer = int;
-def dateTime = date-time;
 
+#####Decorators for Tags and Tag Groups
+#####apiID, name overlaps with both Tags and Tag Groups
+def Color(fn,TagColor):
+    fn.__TagColor = TagColor
+    return fn
+
+def Name(fn, name):
+    fn.__name = name
+    return fn
+
+def Point(fn, point):
+    fn.__point = point
+    return fn
+
+def DateCreated(fn, dateCreated):
+    fn.__dateCreated = dateCreated
+    return fn
+
+def Id(fn, apiID):
+    fn.__apiID = apiID
+    return fn
+
+
+def ImageId(fn, imageid):
+    fn.__imageid = imageid
+    return fn
+
+def LastModified(fn, lastModified):
+    fn.__lastModified = lastModified
+    return fn
+
+def DateCreated(fn, dateCreated):
+    fn.__dateCreated = dateCreated
+    return fn
+
+def User(fn, user):
+    fn_user = user
+    return fn
+
+def IsPrivate(fn, isPrivate = true):
+    fn.__isPrivate = isPrivate
+    return fn
