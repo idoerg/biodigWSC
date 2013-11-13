a29016cdec964412b4e593e7fde988b5d4c6200f
billcomminos <billcomminos@BioDIG.(none)>
2013-11-13 12:41:08 -0500

    Changed Model. Should be correctly working now.

--- python/BioDIG/swagger/decorators/Model.py
+++ python/BioDIG/swagger/decorators/Model.py
@@ -1,69 +1,58 @@
 #!usr/bin/python
 
-##Used to import the Operations class and the methods there
-import Operations
-Operation = Operations.Operation()
-
 #This section was based off of the Swagger Specification of Data Types under the Complex Types section
 
 #Decorator that is used when you define a more complex structure
 def Property(name, typ):
     def inner(fn):
-        if not hasattr(fn, '__operation'):
-            fn.__operation = Operation()
+        if not hasattr(fn, '__model'):
+            fn.__model = Model()
             
-        fn.__attribute.setName(name)
-        fn.__name = name;
+        attribute = Attribute()
+        attribute.setName(name)
+        attribute.setType(typ)
+        model.addAttribute(attribute)
         
-        fn.__attribute.setType(typ)
-        fn.__typ = typ;
         return fn
     return inner
 
 #Used to define the name
-def Name(name):
+def Description(desc):
     def inner(fn):
-        if not hasattr(fn, '__operation'):
-            fn.__operation = Operation()
+        if not hasattr(fn, '__model'):
+            fn.__model = Model()
             
-        fn.__attribute.setName(name)
-        fn.__name = name;
-        return fn
-    return inner
-
-#Used to define the type of the function
-def Type(tp, name):
-    def inner(fn):
-        if not hasattr(fn, '__operation'):
-            fn.__operation = Operations.Operation()
-            
-        fn.__attribute.setName(name)
-        fn.__type = type;
+        fn.__model.setDescription(desc)
         return fn
     return inner
 
 #Complex decorator that needs the name and the type
 #tp == type, fm == format
-def Id( name, tp, fm = None):
+def Id(ID):
     def inner(fn):
         if not hasattr(fn, '__model'):
             fn.__model = Model()
             
-        fn.__model.setType(tp)
-        fn.__tp = tp;
-        
-        fn.__model.setFormat(fm)
-        fn.__fm = fm;
+        fn.__model.setID(ID)
         return fn
     return inner
 
 class Model(object):
     def __init__(self):
-        self.tp = {} # default value for tp(Type)
+        self.ID = '' # default value for tp(Type)
        
-        self.fm = {} #default value for fm(format)
-       
-        self.name = {}
+        self.description = ''
+
+        self.attributes = {}
+
+    def setID(self, ID):
+        self.ID = ID
+
+    def setDescription(self, desc):
+        self.description = desc
+
+    def setAttribute(self, attr):
+        self.attributes[attr.name] = attr
 
 #Class of Objects for the decorators
 class Attribute(object):
